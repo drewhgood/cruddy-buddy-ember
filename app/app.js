798aa0ad7ef72314d605653d4e4fc6a24c212173
Andrew Good <drewhgood@gmail.com>
2015-04-20 17:01:15 -0700

    commit for amar.

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ import config from './config/environment';
 
 var App;
 
+
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
--- app/routes/users/edit.js
+++ app/routes/users/edit.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+        submitAction: function() {
+            var self = this;
+            this.controller.get('model').save().then( 
+              function() { 
+                //     self.transitionTo('index');
+            });
+        }, 
+
+        destroyUser: function() {
+            // this.controller.get('model').destroy();
+            var self = this.controller.get('model')
+            self.destroy();
+        }
+  }
+
 });
