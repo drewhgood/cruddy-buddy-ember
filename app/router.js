46a50dd9b9dcf1344fe35a197568fa1d223551cd
Anderson Saunders <andsbf@gmail.com>
2015-04-23 11:39:08 -0700

    fix not found imgs and singular instance pages

--- app/router.js
+++ app/router.js
@@ -6,14 +6,14 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.resource('users', function() {
-    this.route('show', { path: '/:user_id' });
-    this.route('edit', { path: '/:user_id/edit' });
-    this.route('new');
-  });
+  // this.resource('users', function() {
+  //   this.route('show', { path: '/:user_id' });
+  //   this.route('edit', { path: '/:user_id/edit' });
+  //   this.route('new');
+  // });
 
   this.resource('admin', function() {
-
+     
     this.resource('employees', function(){
       this.route('new');
       this.route('show', { path: '/:id' });
