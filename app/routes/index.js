be96dadbbac523c0cd688d0f29d8d6c4e7bed291
Anderson Saunders <andsbf@gmail.com>
2015-04-23 15:07:12 -0700

    errors fix and create payment model

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-        return {employee: this.store.find('employee'),user: this.store.find('user')};
+        return {employee: this.store.find('employee'), payment: this.store.find('payment')};
 // return this.store.find('employee');
   }
 });
---
+++ app/routes/payments/show.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    return {payment: this.store.find('payment',params.id)};
+  }
+});
