ab5192a4d9bed820a43539c00fcd6dec495167bd
amarbirgill <amarbir_151@hotmail.com>
2015-04-24 14:43:41 -0700

    re-nest route under show

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
-import Ember from 'ember';
+//   import Ember from 'ember';
 
-export default Ember.Route.extend({
-  model: function() {
-        return {employee: this.store.find('employee'), payment: this.store.find('payment')};
-// return this.store.find('employee');
-  }
-});
+// export default Ember.Route.extend({
+//   model: function() {
+//         return {employee: this.store.find('employee'), payment: this.store.find('payment')};
+// // return this.store.find('employee');
+//   }
+// });
