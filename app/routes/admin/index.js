840e4b0d0242ffff3a66c3c6402bcb222774cebc
amarbirgill <amarbir_151@hotmail.com>
2015-04-27 21:57:16 -0700

    dependat registration

--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  setupController: function(controller, client){
-    controller.set('model', client.get('client'));
-  }
-
    model: function() {
     return {dashboards: this.store.find('dashboard')};
   },  
