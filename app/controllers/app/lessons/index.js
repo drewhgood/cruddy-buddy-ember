b781a44e59f82f7f30bf3bcbd659a723c6c08463
Andrew Good <drewhgood@gmail.com>
2015-04-27 18:09:50 -0700

    basic registration form complete.

--- app/controllers/app/lessons/index.js
+++ app/controllers/app/lessons/index.js
@@ -15,11 +15,11 @@ export default Ember.Controller.extend({
       $('#cart h2').css('padding-top', '55px');
     },
 
-    expandCart: function(){
-      this.set('properties.checkingOut' , true);
-      $('#cart').css('height', '100%');
-      $('#cart').css('padding-top', '55px');
-      $('#cart h2').css('padding-top', '55px');
+    contractCart: function(){
+      this.set('properties.checkingOut' , false);
+      $('#cart').css('height', '250px');
+      $('#cart').css('padding-top', '0px');
+      $('#cart h2').css('padding-top', '0px');
     }
   },
 
